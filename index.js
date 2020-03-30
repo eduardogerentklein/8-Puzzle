60e57389bea14061b751f8724ec70a68c0544e07
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2020-03-30 14:17:23 -0300

    Updating index.js

--- index.js
+++ index.js
@@ -5,37 +5,23 @@ const { table, getBorderCharacters } = require('table')
 
 const consoleColor = {
     cyan: '\x1b[36m%s\x1b[0m',
-    yellow: '\x1b[33m%s\x1b[0m'
+    yellow: '\x1b[33m%s\x1b[0m',
+    red: '\x1b[31m'
 }
 
 const gameConfig = JSON.parse(fs.readFileSync('config/gameConfig.json', 'utf8'))
 const initialState = JSON.parse(gameConfig.initialState)
-
+let data = matrixjs(initialState)
 //const initialState = [ [0, 1, 3], [4, 2, 5], [7, 8, 6] ];
 //const initialState = [ [1, 2, 0], [4, 5, 3], [7, 8, 6] ];
-const finalState = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
 
 let generation = 0
 let movements = []
 let possibleMoves = []
-
 let previousIndex = 0
 let previousGeneration = []
 
-let data = matrixjs(initialState)
-
-function findBlankPosition(matrix) {
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (matrix[i][j] == 0) {
-                return [i, j];
-            }
-        }
-    }
-}
-
-exports.findValuePosition = findValuePosition
-function findValuePosition(matrix, value) {
+const findValuePosition = (matrix, value) => {
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             if (matrix[i][j] == value) {
@@ -45,9 +31,9 @@ function findValuePosition(matrix, value) {
     }
 }
 
-const moveToRight = (matrix) => {
+const moveToRight = matrix => {
     let result = matrixjs(matrix)
-    const blankPosition = findBlankPosition(matrix)
+    const blankPosition = findValuePosition(matrix, 0)
     const blankPositionY = blankPosition[0];
     const blankPositionX = blankPosition[1];
 
@@ -63,11 +49,10 @@ const moveToRight = (matrix) => {
         return result()
     }
 }
-exports.moveToRight = moveToRight
 
-const moveToLeft = (matrix) => {
+const moveToLeft = matrix => {
     let result = matrixjs(matrix)
-    const blankPosition = findBlankPosition(matrix)
+    const blankPosition = findValuePosition(matrix, 0)
     const blankPositionY = blankPosition[0];
     const blankPositionX = blankPosition[1];
 
@@ -83,11 +68,10 @@ const moveToLeft = (matrix) => {
         return result()
     }
 }
-exports.moveToLeft = moveToLeft
 
-const moveToUp = (matrix) => {
+const moveToUp = matrix => {
     let result = matrixjs(matrix)
-    const blankPosition = findBlankPosition(matrix)
+    const blankPosition = findValuePosition(matrix, 0)
     const blankPositionY = blankPosition[0];
     const blankPositionX = blankPosition[1];
 
@@ -103,11 +87,10 @@ const moveToUp = (matrix) => {
         return result()
     }
 }
-exports.moveToUp = moveToUp
 
-const moveToDown = (matrix) => {
+const moveToDown = matrix => {
     let result = matrixjs(matrix)
-    const blankPosition = findBlankPosition(matrix)
+    const blankPosition = findValuePosition(matrix, 0)
     const blankPositionY = blankPosition[0];
     const blankPositionX = blankPosition[1];
 
@@ -123,9 +106,8 @@ const moveToDown = (matrix) => {
         return result()
     }
 }
-exports.moveToDown = moveToDown
 
-function addMoves(moves) {
+const addMoves = moves => {
     if (movements.length === 0) {
         for (let i = 0; i < moves.length; i++) {
             movements.push(moves[i])
@@ -139,7 +121,7 @@ function addMoves(moves) {
     }
 }
 
-function existMovement(move) {
+const existMovement = move => {
     for (let i = 0; i < movements.length; i++) {
         let equal = true; 
         for (let j = 0; j < 3; j++) {
@@ -156,10 +138,8 @@ function existMovement(move) {
 
     return false;
 }
-
-const compare = (a, b) => a.f > b.f ? 1 : -1;
-
-function bestOfGeneration(possibleMoves) {
+ 
+const bestOfGeneration = possibleMoves => {
     const finalPositions = [[[0, 0], [0, 1], [0, 2]], [[1, 0], [1, 1], [1, 2]], [[2, 0], [2, 1], [2, 2]]];
 
     let g = generation + 1;
@@ -185,7 +165,8 @@ function bestOfGeneration(possibleMoves) {
 
     generation++;
 
-    const ordenedNodes = nodes.sort(compare)
+    const sortAsc = (a, b) => a.f > b.f ? 1 : -1
+    const ordenedNodes = nodes.sort(sortAsc)
 
     for (let i = 0; i < ordenedNodes.length; i++) {
         if (!existMovement(ordenedNodes[i].matrix)) {
@@ -197,19 +178,19 @@ function bestOfGeneration(possibleMoves) {
     }
 
     let child = [];
-
     for (let i = previousIndex + 1; i < previousGeneration.length; i++) {
-        if (!existMovement(previousGeneration[i].matrix)) {
-            moveToRight(previousGeneration[i].matrix) != null && child.push(moveToRight(previousGeneration[i].matrix))
-            moveToLeft(previousGeneration[i].matrix) != null && child.push(moveToLeft(previousGeneration[i].matrix))
-            moveToUp(previousGeneration[i].matrix) != null && child.push(moveToUp(previousGeneration[i].matrix))
-            moveToDown(previousGeneration[i].matrix) != null && child.push(moveToDown(previousGeneration[i].matrix))
+        const previousGen = previousGeneration[i]
+        if (!existMovement(previousGen.matrix)) {
+            moveToRight(previousGen.matrix) != null && child.push(moveToRight(previousGen.matrix))
+            moveToLeft(previousGen.matrix) != null && child.push(moveToLeft(previousGen.matrix))
+            moveToUp(previousGen.matrix) != null && child.push(moveToUp(previousGen.matrix))
+            moveToDown(previousGen.matrix) != null && child.push(moveToDown(previousGen.matrix))
             bestOfGeneration(child)
         }
     }
 }
 
-function showResult(initial, result) {
+const showResult = (initial, result) => {
     let config = { border: getBorderCharacters('norc') }
     console.log('-=[  8  P U Z Z L E  ]=-')
     console.log()
@@ -217,33 +198,42 @@ function showResult(initial, result) {
     console.log(consoleColor.yellow, table(initial, config))
     console.log()
     console.log('Passos:')
-    for (let i = 0; i < result.length; i++) {
-        console.log(consoleColor.cyan, table(result[i], config))
-    }
+
+    result.map(move => console.log(consoleColor.cyan, table(move, config)))
 }
 
-let hEsperado = null;
-let paths = []
-while (hEsperado !== 0) {
-
-    if (initialState === finalState) {
-        console.log('O estado inicial é igual ao estado final.')
-    }
-
+const initialMovements = () => {
     moveToRight(data()) != null && possibleMoves.push(moveToRight(data()))
     moveToLeft(data()) != null && possibleMoves.push(moveToLeft(data()))
     moveToUp(data()) != null && possibleMoves.push(moveToUp(data()))
     moveToDown(data()) != null && possibleMoves.push(moveToDown(data()))
-
-    const result = bestOfGeneration(possibleMoves)
-
-    data = matrixjs(result.matrix)
-    paths.push(data())
-
-    hEsperado = result.h;
-
-    addMoves([result.matrix])
-    possibleMoves = [];
 }
 
-showResult(initialState, paths)
+const exportTestFunctions = () => {
+    exports.findValuePosition = findValuePosition
+    exports.moveToRight = moveToRight
+    exports.moveToLeft = moveToLeft
+    exports.moveToUp = moveToUp
+    exports.moveToDown = moveToDown
+}
+
+const init = () => {
+    let hEsperado = null;
+    let paths = []
+    while (hEsperado !== 0) {
+        initialMovements()
+    
+        const result = bestOfGeneration(possibleMoves)
+    
+        data = matrixjs(result.matrix)
+        paths.push(data())
+    
+        hEsperado = result.h;
+    
+        addMoves([result.matrix])
+        possibleMoves = [];
+    }
+    showResult(initialState, paths)
+    exportTestFunctions()
+}
+init()
